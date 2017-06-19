2ef799dbd6885dc44a4a01add604da98af5eade1
sebastienvuong <sebastien.vuong.teamcanada@gmail.com>
2017-06-19 23:23:56 +0000

    Cleaned up previous attempts

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
-import {DataLoader} from "./datafetch.ts";
+import {DataLoader} from "./datafetch";
 
 export class App {
-    users: any; // TO BE ADJUSTED
+    users: Array<Object>;
     
     constructor() {
         this.users = [];
         DataLoader.fetchData()
             .then(data => {
-                console.log('DATA :', data)
-                /*
-                refinedData = data.map(user => {
+                let refinedData = data.results.map(user => {
                     var refinedOutput = {
+                        login: user.login,
                         gender: user.gender,
                         name: user.name,
                         email: user.email,
@@ -18,7 +17,15 @@ export class App {
                     };
                     return refinedOutput;
                 });
-                */    
+                
+                // Capitalizing first letter
+                refinedData.forEach((user,idx) => {
+                    refinedData[idx].name.first = user.name.first.charAt(0).toUpperCase() + user.name.first.slice(1);
+                    refinedData[idx].name.last = user.name.last.charAt(0).toUpperCase() + user.name.last.slice(1);
+                    refinedData[idx].name.title = user.name.title.charAt(0).toUpperCase() + user.name.title.slice(1);
+                })
+                /*Repeted functionality, would usually write a function to capitalize the first letter*/
+                
                 this.users = refinedData;
             });
     }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import {Aurelia} from 'aurelia-framework'
-// import {HttpClient} from 'aurelia-fetch-client';
-import 'fetch';
+import environment from './environment';
 
 export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
-    .developmentLogging();
-    
+    .feature('resources');
+
+  if (environment.debug) {
+    aurelia.use.developmentLogging();
+  }
+
+  if (environment.testing) {
+    aurelia.use.plugin('aurelia-testing');
+  }
+
   aurelia.start().then(() => aurelia.setRoot());
 }
